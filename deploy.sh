129c4b906c358c53bc66397c2ba80cd7582c6bf3
bingoassistance <bingoassistance@gmail.com>
2022-09-26 14:54:29 +0530

    test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
-sudo echo $USER
-sudo dmidecode -s system-serial-number
+sudo echo $USER>>test1.txt
+sudo dmidecode -s system-serial-number>>test1.txt
+sudo echo $(date)>>test1.txt
+git checkout deployment
+git add .
+git commit -m "test"
+
 	
 
