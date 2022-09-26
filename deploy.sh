61da06b4965645e9e56290cf6e15190ae30f5b57
bingoassistance <bingoassistance@gmail.com>
2022-09-26 14:49:23 +0530

    details

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 USER= sudo echo $USER
 MACHINE=  sudo dmidecode -s system-serial-number
 DATE= sudo echo $(date)
-sudo echo $USER $MACHINE $DATE >>rnd.txt
+sudo echo $USER >>rnd.txt 
+sudo echo $MACHINE >>rnd.txt
+sudo echo $DATE >>rnd.txt
 git checkout deployment
 git add .
 git commit -m "details"
