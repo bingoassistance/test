782b3711a96abfe36bcd4a0a00412f132fb402a4
bingoassistance <bingoassistance@gmail.com>
2022-09-26 14:48:06 +0530

    details

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
-sudo echo $USER
-sudo dmidecode -s system-serial-number
+USER= sudo echo $USER
+MACHINE=  sudo dmidecode -s system-serial-number
+DATE= sudo echo $(date)
+sudo echo $USER $MACHINE $DATE >>rnd.txt
+git checkout deployment
+git add .
+git commit -m "details"
+
+
 	
 
